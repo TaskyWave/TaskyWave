261cf4034984dc33893284ff95bb68afbe832317
clement-dionne <dionne.clement@ik.me>
2024-06-06 22:59:55 +0200

    upadte

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,7 +14,7 @@ export function Sidebar({ setActivePanel, userRole }) {
         <li><a href="#" onClick={() => setActivePanel('settings')}>⚙️ Paramètres</a></li>
       </ul>
       <div className='versionText'>
-        <p>Version-0.0.1-ALPHA-1-build-4</p>
+        <p>Version-0.0.1-ALPHA-2-build-5</p>
       </div>
     </div>
   );
